5bc5cf165cb3e91c462e34892671cb214147bcd8
hyeon8571 <w01085914442@gmail.com>
2023-08-01 17:31:59 +0900

    8.1 commit

---
+++ src/main/java/shop/mtcoding/bank/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package shop.mtcoding.bank.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import shop.mtcoding.bank.domain.user.UserEnum;
+
+@Configuration
+public class SecurityConfig {
+    private final Logger log = LoggerFactory.getLogger(getClass()); // 테스트하기 편하게 하기 위해 @Slf4j 대신 사용
+
+    @Bean // IoC 컨테이너에 BCryptPasswordEncoder() 객체가 등록됨
+    public BCryptPasswordEncoder passwordEncoder() {
+        log.debug("디버그 : BCryptPasswordEncoder 빈 등록 완료");
+        return new BCryptPasswordEncoder();
+    }
+
+    // JWT 필터 등록 필요
+
+    // JWT 서버를 만들 예정, Session 사용안함.
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        log.debug("디버그 : filterChain 빈 등록됨");
+        http.headers().frameOptions().disable(); // iframe 허용안함.
+        http.csrf().disable(); // enable이면 post맨 작동안함
+        http.cors().configurationSource(configurationSource());
+
+        // jSessionId를 서버쪽에서 관리안하겠다는 뜻
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // react, 앱으로 요청할 예정
+        http.formLogin().disable();
+        // httpBasic은 브라우저가 팝업창을 이용해서 사용자 인증을 진행한다.
+        http.httpBasic().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/api/s/**").authenticated()
+                .antMatchers("/api/admin/**").hasRole(""+UserEnum.ADMIN) // 최근 공식문서에서는 ROLE_ 안붙여도 됨
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+
+    public CorsConfigurationSource configurationSource() {
+        log.debug("디버그 : configurationSource cors 설정이 SecurityFilterChain에 등록됨");
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*"); // GET, POST, PUT,DELETE (JavaScript 요청 허용)
+        configuration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용 (프론트엔드 IP만 허용)
+        configuration.setAllowCredentials(true); // 클라이언트에서 쿠키 요청 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
